5f0f7f261db3adfd08700950663cad51e9fe05a1
GillianoA <agard.gilliano@gmail.com>
2023-12-29 21:47:13 -0400

    Update script
    Full script functionality update ability to sort downloads folder added

--- deScript.py
+++ deScript.py
@@ -9,14 +9,16 @@ import zipfile
 # TODO: ADD function to check if any changes were made to dir before processing
 # TODO: ADD check for Dir and if directory zip tree and move to desired location
 # TODO: ADD check for file extension that are not supported by the software
+# TODO: Refactor code to remove code duplication
 
 SCRIPT_DIR_IGNORE = ["\\deFileStress", "Downloads"]
-SCRIPT_DIRS_IGNORE = ["deFileStress", "installers","zips","unknown"]
+SCRIPT_DIRS_IGNORE = ["deFileStress", "installers","zips_folders","unknown"]
 SCRIPT_DOCUMENT_EXTENSIONS = [".pdf",".txt",".pptx",".docx",".xlsx",".xml"]
 SCRIPT_PHOTO_EXTENSIONS = [".jpg",".png",".jpeg",".gif",".psd"]
 SCRIPT_MUSIC_EXTENSIONS = [".mp3",".wav",".flac",".opus"]
 SCRIPT_VIDEO_EXTENSIONS = [".mp4",".flv",".wmv",".mkv",".avchd",".mov",".webm",".avi"]
-SCRIPT_SPECIAL_EXTENSIONS = [".exe",".zip"]
+SCRIPT_ARCHIVED_EXTENSIONS = [".zp",".zip",".rar","gz"]
+SCRIPT_INSTALLER_EXTENSIONS = [".exe",".msp",".msi",".apk"]
 
 # Function to get download path
 def getDownloadPath():
@@ -39,12 +41,33 @@ def findAllDirPaths(source):
         break
     return dirPaths
 
-# zips all found folders
-def zipDirs(filePath):
-    fileName = os.path.basename(filePath)
-    print(fileName)
-    shutil.make_archive(fileName, 'zip', filePath)
+# Gets the names of the files at the path specified
+def getDirNames(source, dirPaths):
+    dirNames = []
     
+    for dirPath in dirPaths:
+            dirName = dirPath.replace(source + "\\", "")
+            dirNames.append(dirName)
+            
+    return dirNames
+
+# zips all found folders
+def getTargetDir(src, destName):
+    newPath = os.path.join(src, destName)
+    return newPath
+
+# Create directory
+def createDir(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+# copies files to new location
+def copyAndOverwrite(source, destination):
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    shutil.copytree(source, destination)
+    shutil.rmtree(source)
+
 # Finds paths to files to be transferred
 def findAllFilePaths(source, extensions):
     filePaths = []
@@ -77,37 +100,84 @@ def main():
         
         dlPath = getDownloadPath()
         
+        # Looks for folders that arent the preprocessed ones
         dirPaths = findAllDirPaths(dlPath)
+        zipPaths = findAllFilePaths(dlPath, SCRIPT_ARCHIVED_EXTENSIONS)
+        exePaths = findAllFilePaths(dlPath, SCRIPT_INSTALLER_EXTENSIONS)
         
-        for directory in dirPaths:
-            zipDirs(directory)
+        # Gets names of files/folders to be transferred to Dir
+        dirNames = getDirNames(dlPath, dirPaths)
+        zipNames = getDirNames(dlPath, zipPaths)
+        exeNames = getDirNames(dlPath, exePaths)
         
-        # # Gets the file paths for all file types
-        # docPaths = findAllFilePaths(dlPath, SCRIPT_DOCUMENT_EXTENSIONS)
-        # photoPaths = findAllFilePaths(dlPath, SCRIPT_PHOTO_EXTENSIONS)
-        # musicPaths = findAllFilePaths(dlPath, SCRIPT_MUSIC_EXTENSIONS)
-        # videoPaths = findAllFilePaths(dlPath, SCRIPT_VIDEO_EXTENSIONS)
+        # Creates the directory fpr transfers
+        zipTargetPath = getTargetDir(dlPath, "zips_folders")
+        createDir(zipTargetPath)
         
-        # # Gets the path to be transferred to
-        # docDestPaths = findDests(docPaths, "Documents")
-        # photoDestPaths = findDests(photoPaths, "OneDrive\\Pictures")
-        # musicDestPaths = findDests(musicPaths, "Music")
-        # videoDestPaths = findDests(videoPaths, "Videos")
+        exeTargetPath = getTargetDir(dlPath, "installers")
+        createDir(exeTargetPath)
         
-        # # Performs the transfer action for each transfer type
-        # for src, dest in zip(docPaths, docDestPaths):
-        #     cutAndPaste(src, dest)
+        # Moves the folders for the new directory
+        for src, dest in zip(dirPaths, dirNames):
+            dirDest = os.path.join(zipTargetPath, dest)
+            copyAndOverwrite(src, dirDest)
         
-        # for src, dest in zip(musicPaths, musicDestPaths):
-        #     cutAndPaste(src, dest)
+        # Moves the zip files to the target directory
+        for src, dest in zip(zipPaths, zipNames):
+            dirDest = os.path.join(zipTargetPath, dest)
+            cutAndPaste(src, dirDest)
         
-        # for src, dest in zip(photoPaths, photoDestPaths):
-        #     cutAndPaste(src, dest)
+        # transfers exe to desired directory
+        for src, dest in zip(exePaths, exeNames):
+            dirDest = os.path.join(exeTargetPath, dest)
+            cutAndPaste(src, dirDest)
+        
+        # Gets the file paths for all file types
+        docPaths = findAllFilePaths(dlPath, SCRIPT_DOCUMENT_EXTENSIONS)
+        photoPaths = findAllFilePaths(dlPath, SCRIPT_PHOTO_EXTENSIONS)
+        musicPaths = findAllFilePaths(dlPath, SCRIPT_MUSIC_EXTENSIONS)
+        videoPaths = findAllFilePaths(dlPath, SCRIPT_VIDEO_EXTENSIONS)
+        
+        # Gets the path to be transferred to
+        docDestPaths = findDests(docPaths, "Documents")
+        photoDestPaths = findDests(photoPaths, "OneDrive\\Pictures")
+        musicDestPaths = findDests(musicPaths, "Music")
+        videoDestPaths = findDests(videoPaths, "Videos")
+        
+        # Performs the transfer action for each transfer type
+        for src, dest in zip(docPaths, docDestPaths):
+            cutAndPaste(src, dest)
+        
+        for src, dest in zip(musicPaths, musicDestPaths):
+            cutAndPaste(src, dest)
+        
+        for src, dest in zip(photoPaths, photoDestPaths):
+            cutAndPaste(src, dest)
             
-        # for src, dest in zip(videoPaths, videoDestPaths):
-        #     cutAndPaste(src, dest)
+        for src, dest in zip(videoPaths, videoDestPaths):
+            cutAndPaste(src, dest)
         
-        # print("File processing complete...")
+        # Looks for remaining file paths
+        remainingPaths = []
+        for root, dirs, files in os.walk(dlPath):
+            for file in files:
+                path = os.path.join(dlPath, file)
+                remainingPaths.append(path)
+            break
+        
+        # Gets remaining file names
+        remainingNames = getDirNames(dlPath, remainingPaths)
+        
+        # Create transfer Directory
+        remainingTargetPath = getTargetDir(dlPath, "unknown")
+        createDir(remainingTargetPath)
+        
+        # Moves remaining files to unknown dir
+        for src, dest in zip(remainingPaths, remainingNames):
+            dirDest = os.path.join(remainingTargetPath, dest)
+            cutAndPaste(src, dirDest)
+        
+        print("File processing complete...")
         time.sleep(6)
 
 if __name__ == '__main__':
